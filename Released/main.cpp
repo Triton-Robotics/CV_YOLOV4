0909736a3d6d4c6db860ba87e73071da75c2379f
Hongbin Miao <59853402+zslrmhb@users.noreply.github.com>
2021-06-18 17:03:31 -0700

    Add files via upload

---
+++ Released/main.cpp
@@ -0,0 +1,29 @@
+// #include <thread>
+// #include "camera.h"
+#include "detector.h"
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <iostream>
+// #include "util.h"
+// #include "MvCameraControl.h"
+
+int main () {
+    Detector detector;
+
+    cv::Mat frame;
+    cv::VideoCapture vid(0); // CHANGE THIS ACCORDINGLY!
+
+    if (!vid.isOpened())
+    {
+         std::cout << "Cannot Open Camera!" << std::endl;
+        return -1;
+    }
+
+    while(vid.read(frame)){
+        std::tuple<float, float> armor_center = detector.DetectLive(frame);
+        // ADD PROCESSING CODE HERE !!
+    }
+    vid.release();
+
+    // destuctor somewhere?
+}
